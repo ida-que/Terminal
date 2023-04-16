f36eb7a271d31182c3b10b6e4e668b962c0d53cd
ida-que <i.am.anna.nikulina@gmail.com>
2023-04-16 17:04:34 +0200

    bash script added

---
+++ homework_1/script.sh
@@ -0,0 +1,11 @@
+FOLDER="/d/terminal_hw1/folder_4"
+
+rm -rf $FOLDER 						&& echo "rm -rf ${FOLDER}" \
+&& mkdir $FOLDER 					&& echo "mkdir ${FOLDER}" \
+&& cd $FOLDER  						&& echo "cd ${FOLDER}" \
+&& mkdir fol1 fol2 fol3 				&& echo "mkdir fol1 fol2 fol3" \
+&& cd fol1  						&& echo "cd fol1" \
+&& touch f1.txt f2.txt f3.txt j1.json j2.json 		&& echo "touch f1.txt f2.txt f3.txt j1.json j2.json" \
+&& mkdir subfol1 subfol2 subfol3 			&& echo "mkdir subfol1 subfol2 subfol3" \
+&& ls -la 						&& echo "ls -la" \
+&& mv -t subfol1 f1.txt f2.txt 				&& echo "mv -t subfol1 f1.txt f2.txt"
